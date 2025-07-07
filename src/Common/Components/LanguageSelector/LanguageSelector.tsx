935a9b89ac9a6150e06fc1da405f3e7bd444e7bf
dmuser <dmitriy.korytnyi@gmail.com>
2025-07-06 22:38:17 -0700

    Exam and some refactoring

--- src/Common/Components/LanguageSelector/LanguageSelector.tsx
+++ src/Common/Components/LanguageSelector/LanguageSelector.tsx
@@ -1,18 +1,33 @@
 import React, { FC } from "react";
 import { FaArrowRight } from "react-icons/fa";
 import styles from "./LanguageSelector.module.scss";
+import classNames from "classnames";
 
 interface ILanguageSelectorProps {
   onSwapLanguages: () => void;
   translationFrom: string;
   translationTo: string;
+  disabled?: boolean;
 }
 
 const LanguageSelector: FC<ILanguageSelectorProps> = (props) => {
+  // const disabled = props.disabled;
+  const handleSwapLanguages = () => {
+    if (props.disabled) {
+      return;
+    }
+    props.onSwapLanguages();
+  };
+
   return (
-    <div className={styles.languageSelector}>
+    <div
+      className={classNames(
+        styles.languageSelector,
+        props.disabled ? styles.disabled : null
+      )}
+    >
       {props.translationFrom}{" "}
-      <FaArrowRight onClick={props.onSwapLanguages} className={styles.ico} />{" "}
+      <FaArrowRight onClick={handleSwapLanguages} className={styles.ico} />{" "}
       {props.translationTo}
     </div>
   );
